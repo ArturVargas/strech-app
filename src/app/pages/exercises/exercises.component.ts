5a7a104c232fa915289b360abd397105d2163405
ArturVargas <arturvargas9226@gmail.com>
2020-09-08 22:57:42 -0500

    create first components

--- src/app/pages/exercises/exercises.component.ts
+++ src/app/pages/exercises/exercises.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercises.component.scss']
 })
 export class ExercisesComponent implements OnInit {
-
+  timeOff: number = 10;
+  timeStrech: number = 45;
+  time = 0;
+  roundCounter = 0;
   constructor() { }
 
   ngOnInit() {
   }
 
+  setTimer(time) {
+    console.log(time);
+    this.time = time;
+    if(time == this.timeStrech) {
+      this.roundCounter ++;
+    }
+    return new Promise((resolve) => {
+      let counter = setInterval(() => {
+        this.time --;
+        if(this.time == 0) {
+          clearInterval(counter);
+          resolve();
+          return;
+        }
+      }, 1000);
+    });
+  }
+
+  // El boton debe deshabilitarse hasta que se termine de ejecutar el ciclo.
+  // El ciclo se puede simplificar de tal manera que el codigo no sea tan redundante.
+
+  async startTimer() {
+    await this.setTimer(this.timeOff);
+    await this.setTimer(this.timeStrech);
+    await this.setTimer(this.timeOff);
+    await this.setTimer(this.timeStrech);
+    await this.setTimer(this.timeOff);
+    await this.setTimer(this.timeStrech);
+  }
 }
